878f9d625b064143cb8ca7e26cfe0a1271e1520e
Artemis Davakou <artemis.davakou@chasacademy.se>
2025-05-07 22:51:47 +0200

    dsadsad

--- recipes/add/index.js
+++ recipes/add/index.js
@@ -1,7 +1,4 @@
-// const recipe = document.getElementById("recipe").value;
-// const recipeTitlePreview = document.getElementById("recipe-title-preview").innerText;
 
-// recipeTitlePreview = recipe;
 
 function previewRecipeImage() {
     const recipeImage = document.getElementById("recipe-image");
@@ -15,26 +12,3 @@ function previewRecipeTitle() {
 }
 
 
-// previewRecipeTitle();
-
-// document.getElementById("recipe-title").addEventListener("input", function() {
-//     const recipe = document.getElementById("recipe");
-//     const recipeTitlePreview = document.getElementById("recipe-title-preview");
-//     recipeTitlePreview.textContent = recipe.value;
-// }   );
-
-function changeTitle() {
-    const formTitleElement = document.getElementById("form-recipe-title");
-    const previewTitleElement = document.getElementById("preview-recipe-title");
-    previewTitleElement.innerText = formTitleElement.value;
-}
-
-function addIngredient() {
-    const ingredientType = document.getElementById("form-ingredient-type");
-    const ingredientQuantity = document.getElementById("form-ingredient-quantity");
-    const ingredientUnit = document.getElementById("form-ingredient-unit");
-    const ingredientsListElement = document.getElementById("preview-recipe-ingredients-list");
-    const ingredientElement = document.createElement("li");
-    ingredientElement.innerText = `${ingredientQuantity.value}${ingredientUnit.innerText} - ${ingredientType.value}`
-    ingredientsListElement.appendChild(ingredientElement);
-}
