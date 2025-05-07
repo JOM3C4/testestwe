67402e36e3fc872fee817c32745837e2a196851c
Artemis Davakou <artemis.davakou@chasacademy.se>
2025-05-07 23:15:16 +0200

    tesst

--- recipes/add/index.js
+++ recipes/add/index.js
@@ -12,3 +12,37 @@ function previewRecipeTitle() {
 }
 
 
+function previewRecipeCategory() {
+    const recipeCategory = document.getElementById("recipe-category");
+    const recipeCategoryPreview = document.getElementById("recipe-category-preview");
+    recipeCategoryPreview.innerText = recipeCategory.options[recipeCategory.selectedIndex].text;
+}
+
+
+
+async function getCategories() {
+    const url = "http://localhost:5001/api/v1/categories";
+    // const url = "https://express-api-jbkl.onrender.com/api/v1/categories";
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+        throw new Error(`Response status: ${response.status}`);
+        }
+    
+        const json = await response.json();
+        const categories = json.data;
+        const categorySelect = document.getElementById("recipe-category");
+    
+        categories.forEach((category) => {
+        const option = document.createElement("option");
+        option.value = category.id;
+        option.innerText = category.name;
+        categorySelect.appendChild(option);
+        });
+    } catch (error) {
+        console.error(error.message);
+    }
+    }
+
+    getCategories();
+
